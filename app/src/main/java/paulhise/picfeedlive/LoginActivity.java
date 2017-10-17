c8b184a49e17f4ab93383b07c5d453c61ad1bd44
Paul <paulhise1@gmail.com>
2017-10-17 16:04:46 -0700

    reconfigured LoginActivity UI xml.

--- app/src/main/java/paulhise/picfeedlive/LoginActivity.java
+++ app/src/main/java/paulhise/picfeedlive/LoginActivity.java
@@ -1,10 +1,14 @@
 package paulhise.picfeedlive;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 // create EditText input field where users can enter their username.
 // establish a shared preferences to store the user name in.
@@ -22,19 +26,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // initializing member variables
+        // calling class methods
+        setUpView();
+
+    }
+
+    private void setUpView(){
+        // initiailizing member variables for setUpView method
         mSignInButton = (Button) findViewById(R.id.button_user_sign_in);
         mCreateUserButton = (Button) findViewById(R.id.button_create_user_login);
         mUserNameCreationInput = (EditText) findViewById(R.id.edit_text_username_input);
 
-        // calling class methods
-        attachOnClickListeners();
+        mUserNameCreationInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                // detect if the user presses [enter]
+                if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    saveUsername();
+                    return true;
+                }
+                return false;
+            }
+        });
 
-
-    }
-
-    private void attachOnClickListeners(){
-
+        // Button listeners and logic for buttons
         mSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,7 +63,14 @@ public class LoginActivity extends AppCompatActivity {
                 // write logic for mCreateUserButton
             }
         });
+    }
 
+    private void saveUsername(){
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
 }
